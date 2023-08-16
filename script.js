d7f5d010e8420a49b9c88f6de297d5d45a4675fe
MorgynKass <morgynkass@gmail.com>
2023-08-15 18:52:10 -0600

    Added comments to my js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+//Created a variable named "URL" and assigned it to a string that contains a url.
 const URL =
 "https://iskarr.github.io/austindonovan.github.io/api/business.json";
 
+//Created two variables named "" and "". Then assigned them to their corresponing elements in the HTML document. 
 let cards = document.querySelector("div.cards");
 let para = document.createElement("p");
 
+//Used the method "fetch()" to extract url and assigned the information to a varible. Then created a for loop to console.log each object.
 fetch(URL)
     .then((response) => response.json())
     .then((jsObject) => {
